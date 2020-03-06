8b972bf05e788ba62b7621a692964f24d849934d
root <root@ip-10-3-53-2.ec2.internal>
2020-03-06 16:07:42 +0000

    Initial commit

---
+++ central-bundle/central/scripts/route-setup.sh
@@ -0,0 +1,7 @@
+
+#!/usr/bin/env bash
+# This route will be accessible through central-stackrox.your-cluster-subdomain.
+oc create route passthrough central -n stackrox --service central
+# This route allows other clusters to connect via mutual TLS with the
+# Server Name Indication "central.stackrox".
+oc create route passthrough central-mtls -n stackrox --hostname=central.stackrox --service central
---
+++ central-bundle/scanner/scripts/setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+KUBE_COMMAND=${KUBE_COMMAND:-oc}
+
+${KUBE_COMMAND} get namespace stackrox &>/dev/null || ${KUBE_COMMAND} create namespace stackrox
+${KUBE_COMMAND} -n stackrox annotate namespace/stackrox --overwrite openshift.io/node-selector=""
+
+
+
+${KUBE_COMMAND} get scc/scanner &>/dev/null || ${KUBE_COMMAND} create -f "$DIR/scanner-scc.yaml.txt"
+while ! ${KUBE_COMMAND} get scc/scanner &>/dev/null; do
+    sleep 1
+done
+
+if ! ${KUBE_COMMAND} get secret/stackrox -n stackrox > /dev/null; then
+  registry_auth="$("${DIR}/../../docker-auth.sh" -m k8s "https://10.3.84.210:5000")"
+  [[ -n "$registry_auth" ]] || { echo >&2 "Unable to get registry auth info." ; exit 1 ; }
+  ${KUBE_COMMAND} create --namespace "stackrox" -f - <<EOF
+apiVersion: v1
+data:
+  .dockerconfigjson: ${registry_auth}
+kind: Secret
+metadata:
+  name: stackrox
+  namespace: stackrox
+  labels:
+    app.kubernetes.io/name: stackrox
+type: kubernetes.io/dockerconfigjson
+EOF
+fi
